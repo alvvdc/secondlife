3272195438116188800c28c49dc2091c05338e82
Alberto <albertodtres@gmail.com>
2020-01-19 22:27:58 +0100

    Toolbar terminado

--- frontend/app/src/main/java/com/iesvirgendelcarmen/secondlife/ui/MainActivity.kt
+++ frontend/app/src/main/java/com/iesvirgendelcarmen/secondlife/ui/MainActivity.kt
@@ -2,19 +2,30 @@ package com.iesvirgendelcarmen.secondlife.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.navigation.NavigationView
 import com.iesvirgendelcarmen.secondlife.R
 import com.iesvirgendelcarmen.secondlife.model.ProductViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+
+
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private val productViewModel: ProductViewModel by lazy {
         ViewModelProviders.of(this).get(ProductViewModel::class.java)
     }
 
     lateinit var drawerLayout : DrawerLayout
+    lateinit var toolbar: Toolbar
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +38,23 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }
 
+        toolBar()
+    }
+
+    private fun toolBar() {
         drawerLayout = findViewById(R.id.drawer_layout)
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_menu)
+        setSupportActionBar(toolbar)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
 
 
-
+        return true
     }
 }
