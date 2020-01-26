5a46bcd6461c901c00e884f37883f3d61a6412f3
Álvaro García Muñoz <alvvdc@gmail.com>
2020-01-26 08:32:30 +0100

    Actualizado Product, la lista de productos inicial y la conexión para que corresponda con el backend
    	modified:   frontend/.idea/misc.xml
    	modified:   frontend/app/src/main/java/com/iesvirgendelcarmen/secondlife/config/APIConfig.kt
    	modified:   frontend/app/src/main/java/com/iesvirgendelcarmen/secondlife/model/Product.kt
    	modified:   frontend/app/src/main/java/com/iesvirgendelcarmen/secondlife/model/ProductRecyclerViewAdapter.kt
    	modified:   frontend/app/src/main/java/com/iesvirgendelcarmen/secondlife/model/ProductViewModel.kt

--- frontend/app/src/main/java/com/iesvirgendelcarmen/secondlife/model/ProductViewModel.kt
+++ frontend/app/src/main/java/com/iesvirgendelcarmen/secondlife/model/ProductViewModel.kt
@@ -11,20 +11,20 @@ class ProductViewModel : ViewModel() {
     private val productRepository :ProductRepositoryDataSource = ProductRepositoryVolley
 
     init {
-        mock()
+        //mock()
     }
 
     private fun mock() {
-        productsList.add(Product(1, 1,"Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
-        productsList.add(Product(1, 1, "Mueble rústico", "", 130f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
-        productsList.add(Product(1, 1, "Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
-        productsList.add(Product(1, 1, "Mueble rústico", "", 130f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
-        productsList.add(Product(1, 1, "Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
-        productsList.add(Product(1, 1, "Mueble rústico", "", 130f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
-        productsList.add(Product(1, 1, "Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
-        productsList.add(Product(1, 1, "Mueble rústico", "", 130f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
-        productsList.add(Product(1, 1, "Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
-        productsList.add(Product(1, 1, "Mueble rústico", "", 130f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
+        productsList.add(Product("1", "1","Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
+        productsList.add(Product("1", "1", "Mueble rústico", "", 232f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
+        productsList.add(Product("1", "1", "Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
+        productsList.add(Product("1", "1", "Mueble rústico", "", 232f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
+        productsList.add(Product("1", "1", "Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
+        productsList.add(Product("1", "1", "Mueble rústico", "", 232f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
+        productsList.add(Product("1", "1", "Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
+        productsList.add(Product("1", "1", "Mueble rústico", "", 232f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
+        productsList.add(Product("1", "1", "Muchas camisetas", "Están nuevas a estrenar, no hago envios.", 20f, mutableListOf("https://i.imgur.com/0oPAfru.png")))
+        productsList.add(Product("1", "1", "Mueble rústico", "", 232f, mutableListOf("https://i.imgur.com/rWkb5AJ.png")))
     }
 
     fun getAllProducts() {
